e9f5deaa70f20c10ca6025b4df0c6716f740a9ce
Md Kamran <mdkamran7255@gmail.com>
2024-02-22 15:15:39 +0530

    refac: add react-web-hook form

--- src/components/startGame/startGame.tsx
+++ src/components/startGame/startGame.tsx
@@ -2,40 +2,62 @@
 import { useRouter } from "next/navigation";
 import React, { useContext, useState } from "react";
 import LoadingButton from "../loadingButton/loadingButton";
-import Input from "../input/input";
 import { joinRoom, room } from "@/constants/apiUrl";
 import { GameContext } from "@/context/gameContext";
 import ToastConainer from "../toastConainer";
 import { showErrorToast } from "../../utils/toast";
-import socket from "../../utils/socket"
+import socket from "../../utils/socket";
+import { useForm, SubmitHandler, Resolver } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+interface JoinRoomState {
+  joinRoomCode: number;
+}
+
+const schema = yup
+  .object()
+  .shape({
+    joinRoomCode: yup
+      .number()
+      .typeError("Room code is required to join the room")
+      .test("is-6-digits", "Enter 6 digit room code", (value) => {
+        if (value) {
+          return value.toString().length === 6;
+        }
+        return true;
+      }),
+  })
+  .required();
 
 const StartGame = ({ userData }: { userData: UserResponseData }) => {
   const { game, setGame } = useContext(GameContext) as GameContextType;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<JoinRoomState>({
+    resolver: yupResolver(schema) as Resolver<JoinRoomState>,
+  });
 
   const [loading, setLoading] = useState({
     createRoom: false,
     joinRoom: false,
   });
-  const [joinRoomCode, setJoinRoomCode] = useState<string>("");
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setJoinRoomCode(e.target.value);
-  };
   const router = useRouter();
 
   const createRoom = async () => {
     setLoading({ ...loading, createRoom: true });
-  
+
     try {
       const customRoomCode = Math.random().toString().substring(2, 8);
-  
+
       const newGame = {
         roomCode: customRoomCode,
         playerXId: userData.data._id,
       };
-  
+
       const res = await fetch(room, {
         method: "POST",
         headers: {
@@ -43,10 +65,10 @@ const StartGame = ({ userData }: { userData: UserResponseData }) => {
         },
         body: JSON.stringify({ game: newGame }),
       });
-  
+
       const data = await res.json();
       if (res.ok) {
-        socket.emit("joinSocketChannel", customRoomCode)
+        socket.emit("joinSocketChannel", customRoomCode);
         router.push(`/room/${customRoomCode}`);
       } else {
         showErrorToast(data.message, "error");
@@ -59,50 +81,51 @@ const StartGame = ({ userData }: { userData: UserResponseData }) => {
         console.error("Error creating room:", error.message);
       }
     }
-  };  
+  };
 
-  const handleJoinRoom = async () => {
-    if (joinRoomCode && joinRoomCode.length === 6) {
-      setLoading({ ...loading, joinRoom: true });
+  const handleJoinRoom: SubmitHandler<JoinRoomState> = async (room) => {
+    setLoading({ ...loading, joinRoom: true });
+
+    try {
+      const res = await fetch(`${joinRoom}/${room.joinRoomCode}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          status: "true",
+          playerOId: userData.data._id,
+        }),
+      });
+
+      const data = await res.json();
+      if (!res.ok) {
+        showErrorToast(data.message, "error");
+        setLoading({ ...loading, joinRoom: false });
+        return;
+      }
       setGame({
         ...game,
         status: true,
         playerOId: userData.data._id,
       });
-      try {
-        const res = await fetch(`${joinRoom}/${joinRoomCode}`, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            status: "true",
-            playerOId: userData.data._id,
-          }),
-        });
+      socket.emit("joinSocketChannel", room.joinRoomCode);
 
-        const data = await res.json();
-        if (!res.ok) {
-          showErrorToast(data.message, "error");
-          setLoading({ ...loading, joinRoom: false });
-          return;
-        }
-        socket.emit("joinSocketChannel", joinRoomCode)
-
-        socket.emit("joinGame", {
-          status: true,
-          playerOId: userData.data,
-          roomCode: joinRoomCode,
-        });
-        router.push(`/room/${joinRoomCode}`);
-      } catch (error) {
-        setLoading({ ...loading, joinRoom: false });
-        if (error instanceof Error) {
-          console.error(error.message);
-        }
+      socket.emit("joinGame", {
+        status: true,
+        playerOId: userData.data,
+        roomCode: room.joinRoomCode,
+      });
+      router.push(`/room/${room.joinRoomCode}`);
+    } catch (error) {
+      setLoading({ ...loading, joinRoom: false });
+      if (error instanceof Error) {
+        console.error(error.message);
       }
     }
   };
+
+  console.log(errors.joinRoomCode);
 
   return (
     <>
@@ -115,31 +138,28 @@ const StartGame = ({ userData }: { userData: UserResponseData }) => {
           {loading.createRoom ? <LoadingButton /> : "Create Room"}
         </button>
       </div>
-      <div className="flex flex-col px-2 w-full md:w-[20rem] justify-center items-center">
-        <div className="w-[15rem] md:w-full relative">
-          <Input
-            key={"joinRoom"}
-            handleChange={handleChange}
-            value={joinRoomCode}
-            labelText={"Enter Room Code"}
-            labelFor={"joinRoom"}
-            id={"joinRoom"}
-            name={"joinRoomCode"}
-            type={"text"}
-            isRequired={true}
-            placeholder={"Enter Room Code"}
-          />
-        </div>
+      <form onSubmit={handleSubmit(handleJoinRoom)}>
+        <div className="flex flex-col px-2 w-full md:w-[20rem] justify-center items-center">
+          <div className="w-[15rem] md:w-full relative">
+            <input
+              className="inputClass"
+              {...register("joinRoomCode")}
+              placeholder="Enter Room Code here..."
+            />
+            <p className="text-red-600">{errors.joinRoomCode?.message}</p>
+          </div>
 
-        <div className="w-full">
-          <button
-            className="group mx-auto md:w-[10rem]  break-keep flex justify-center cursor-pointer py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
-            onClick={handleJoinRoom}
-          >
-            {loading.joinRoom ? <LoadingButton /> : "Join Room"}
-          </button>
+          <div className="w-full">
+            <button
+              type="submit"
+              className="group mt-2 mx-auto md:w-[10rem]  break-keep flex justify-center cursor-pointer py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+            >
+              {loading.joinRoom ? <LoadingButton /> : "Join Room"}
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
+
       <ToastConainer />
     </>
   );
